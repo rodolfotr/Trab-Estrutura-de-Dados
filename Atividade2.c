ad1abd9d19408c7f8d3f1108826967edde2d2228
rodolfotr <rodolfotrevisol015@gmail.com>
2016-03-10 09:42:54 -0300

    Update Atividade2.c

--- Atividade2.c
+++ Atividade2.c
@@ -1,51 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-
-
-int* uniao( int *v1, int n1, int *v2, int n2){
+int *uniao (int *v1, int n1, int *v2, int n2){
     int x, *y, i;
-    
-    x= n1 + n2;
-    
-    y=(int*)calloc(x * sizeof(int));
-    
-    for(i=0; i<n1; i++){
-    y[i]=v1[i];
-}
-    for(i=0; i<n2; i++){
-    y[i+n1]=v2[i];
-}
+
+    x = n1 + n2;
+
+    y =(int*)calloc(x, sizeof(int));
+
+    for(i=0; i<n1; i++)
+        y[i]=v1[i];
+
+    for(i=0; i<(n2); i++)
+        y[i+n1]=v2[i];
+
 
 return y;
-}
+};
 
 int main (){
 
 
 int *v1, *v2, *v3;
-int n1, n2, i, j, k;
+int n1, n2, i;
 
-printf("QUAL O NUMERO DE ELEMENTOS DO VETOR 1: \n");
-scanf("%d", &n1);
-printf("QUAL O NUMERO DE ELEMENTOS DO VETOR 2: \n");
-scanf("%d", &n2);
+    srand( (unsigned)time(NULL) );
 
-v1 = (int *) calloc (n1 * sizeof(int));
-v2 = (int *) calloc (n2 * sizeof(int));
+    printf("QUAL O NUMERO DE ELEMENTOS DO VETOR 1: \n");
+    scanf("%d", &n1);
+    printf("QUAL O NUMERO DE ELEMENTOS DO VETOR 2: \n");
+    scanf("%d", &n2);
 
-for(i=0; i<n1; i++){
-    v1[i]=rand()%100;
-}
-for(i=0; i<n2; i++){
-    v2[i]=rand()%100;
-}
-
-int* uniao( int *v1, int n1, int *v2, int n2);
+    v1 = (int *) calloc (n1, sizeof(int));
+    for(i=0; i<n1; i++)
+        v1[i]=rand()%100;
 
 
+    v2 = (int *) calloc (n2, sizeof(int));
+    for(i=0; i<n2; i++)
+        v2[i]=rand()%100;
 
+    v3 = uniao(v1, n1, v2 ,n2);
 
-free(Var);
-return 0;
+    free(v1);
+    free(v2);
+
+    printf("\nVETOR UNIDO\n");
+    for(i=0;i<(n1+n2); i++){
+        printf("%d ", v3[i]);
+    }
+
+    return 0;
 }
