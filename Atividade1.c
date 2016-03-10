30711442dddaedc8e533f3f251eccacb46108b2c
rodolfotr <rodolfotrevisol015@gmail.com>
2016-03-09 21:59:43 -0300

    Create Atividade1.c

---
+++ Atividade1.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdlib.h>
+
+int main (){
+
+int x=0, i, y=5, c=0;
+
+int *Var = (int *)malloc (y * sizeof(int));
+
+if(Var == NULL){
+	printf("ERRO DE ALOCACAO DE MEMORIA");
+	}
+else {
+	for (i=0; i<=y; i++){
+		printf("ENTRE COM UM INTEIRO: \n");
+		if(scanf("%d", &x)==0){
+			printf("GO TO HELL MOTAFOKA\n");
+			break;
+		}
+		else
+            if(i == y){
+                y += 5;
+                Var = (int *) realloc (Var, y * sizeof(int));
+		}
+		Var[i] = x;
+		c++;
+
+	}
+}
+    printf("\n\n\nVALORES DIGITADOS: \n\n");
+    for (i=0; i<c; i++){
+        printf("%d\n", Var[i]);
+    }
+
+free(Var);
+return 0;
+}
